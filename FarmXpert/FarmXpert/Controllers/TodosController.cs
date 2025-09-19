e634a980012a6ca18aecc8a5d3761e90d8371b3b
Sergiu Gherasim <sergeras4@gmail.com>
2025-09-19 21:01:58 +0300

    feat(Server): add TodosController with CRUD operations for Todo items

---
+++ FarmXpert/FarmXpert/Controllers/TodosController.cs
@@ -0,0 +1,72 @@
+using FarmXpert.Application.Todo.Commands.CreateTodo;
+using FarmXpert.Application.Todo.Commands.DeleteTodo;
+using FarmXpert.Application.Todo.Commands.UpdateTodo;
+using FarmXpert.Application.Todo.Queries.GetAllTodos;
+using FarmXpert.Application.Todo.Queries.GetTodoById;
+using FarmXpert.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmXpert.Controllers;
+
+[ApiController]
+[Route("api/todos")]
+public class TodosController: ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public TodosController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        List<Todo> todos = await _mediator.Send(new GetAllTodosQuery());
+        return Ok(todos);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var todo = await _mediator.Send(new GetTodoByIdQuery(id));
+        if (todo == null)
+        {
+            return NotFound();
+        }
+        return Ok(todo);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateTodoCommand command)
+    {
+        var todo = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), todo, null);
+    }
+    
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(UpdateTodoCommand command)
+    {
+        var updatedTodo = await _mediator.Send(command);
+        if (updatedTodo == null)
+        {
+            return BadRequest("Failed to modify salary.");
+        }
+        
+        return Ok(updatedTodo);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var todo = await _mediator.Send(new GetTodoByIdQuery(id));
+        if (todo == null)
+        {
+            return NotFound();
+        }
+        
+        await _mediator.Send(new DeleteTodoCommand(id));
+        return Ok(todo);
+    }
+}
