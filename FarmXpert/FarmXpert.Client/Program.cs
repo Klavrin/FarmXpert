ff48d37774749c5067b0dc4e5dd27785fca49804
Sergiu Gherasim <sergeras4@gmail.com>
2025-09-23 18:30:35 +0300

    feat(Client): add MudBlazor services in Program.cs

--- FarmXpert/FarmXpert.Client/Program.cs
+++ FarmXpert/FarmXpert.Client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -7,5 +8,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
