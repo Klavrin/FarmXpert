9a1d770e377403809cbceb3efd7d9faee8ea52e9
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:38:45 +0300

    add CropType enum

---
+++ FarmXpert/FarmXpert.Domain/Enums/CropType.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Enums
+{
+    public enum CropType
+    {
+        Wheat,
+        Rice,
+        Maize, // Corn
+        Barley,
+        Oats,
+        Rye,
+        Sorghum,
+        Millet,
+        Soybean,
+        Pea,
+        Lentil,
+        Chickpea,
+        Bean,
+        Sunflower,
+        Rapeseed, // Canola
+        Flax,
+        Groundnut, // Peanut
+        Sesame,
+        Potato,
+        SweetPotato,
+        Cassava,
+        Yam,
+        Beetroot,
+        Carrot,
+        Radish,
+        Tomato,
+        Cucumber,
+        Onion,
+        Garlic,
+        Cabbage,
+        Lettuce,
+        Spinach,
+        Pepper,
+        Eggplant,
+        Pumpkin,
+        Zucchini,
+        Apple,
+        Pear,
+        Grape,
+        Banana,
+        Orange,
+        Lemon,
+        Mango,
+        Pineapple,
+        Strawberry,
+        Watermelon,
+        Melon,
+        Cotton,
+        Jute,
+        Hemp,
+        Coffee,
+        Tea,
+        Cocoa,
+        Sugarcane,
+        Tobacco,
+        Rubber,
+        Olive,
+        Other
+    }
+
+}
