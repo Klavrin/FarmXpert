eede2e4c560a212cc11eeef905cc1476625905cc
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:51:30 +0300

    add Subsidy entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/Subsidy.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Entities
+{
+    public class Subsidy
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public Guid BusinessId { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+        public object AppliesTo { get; set; } = new();
+        public object Deadline { get; set; } = new();
+        public string Category { get; set; }
+        public string Authority { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime OpensAt { get; set; }
+        public DateTime ClosesAt { get; set; }
+        public string Summary { get; set; }
+    }
+}
