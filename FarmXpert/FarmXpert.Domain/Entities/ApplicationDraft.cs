9f0b868b400516cc7246cda116b402335a87f21e
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:49:48 +0300

    add ApplicationDraft entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/ApplicationDraft.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Entities
+{
+    public class ApplicationDraft
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public Guid UserId { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public Guid BusinessId { get; set; }
+        public string SubsidyCode { get; set; }
+        public string Status { get; set; }
+        public List<string> Suggestions { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
