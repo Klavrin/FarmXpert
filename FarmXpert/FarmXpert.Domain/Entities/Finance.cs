f953ae6a284d4e01cd8d99f372cdc9663b2a8ce8
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:51:15 +0300

    add Finance entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/Finance.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Entities
+{
+    public class Finance
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public Guid BusinessId { get; set; }
+        public long YearlyIncome { get; set; }
+        public long YearlyExpenses { get; set; }
+    }
+}
