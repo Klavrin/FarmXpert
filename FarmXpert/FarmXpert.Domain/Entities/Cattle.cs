f9e26b63758b8f5aa3cd3386fa9f6c6eb9f80fca
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:50:41 +0300

    add Cattle entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/Cattle.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Entities
+{
+    public class Cattle
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public Guid BusinessId { get; set; }
+        public string Type { get; set; }
+        public List<Animal> Animals { get; set; }
+        public int Amount { get; set; }
+    }
+}
