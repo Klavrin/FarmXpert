ca6702b107ed17354cc1776b6df4daed9548fc46
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:50:23 +0300

    add ApplicationFile entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/ApplicationFile.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Entities
+{
+    public class ApplicationFile
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        [BsonRepresentation(BsonType.String)]
+        public Guid DraftId { get; set; }
+        public string SrcUrl { get; set; }
+        public string OutPath { get; set; }
+        public string Extension { get; set; }
+    }
+}
