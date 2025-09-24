db7f599256d210239e560ffb0f22c87c7e193dda
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:39:58 +0300

    git add SoilType enum

---
+++ FarmXpert/FarmXpert.Domain/Enums/SoilType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Enums
+{
+    public enum SoilType
+    {
+        CalcareousChernozem,
+        TypicalChernozem,
+        ArgillicChernozem,
+        LeachedChernozem,
+        BrownSoil,
+        ForestGreySoil,
+        AlluvialSoil,
+        MeadowSoil,
+        SalineSoil,
+        MarshSoil,
+        SaltAffectedSoil,
+        Rendzina,
+        Vertisol,
+        Gleysol,
+        Solonetz,
+        Solonchak,
+        Other
+    }
+}
