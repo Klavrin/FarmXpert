7861e448aa3ede0a1035cd3ac6985dc7cce9dc32
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 01:38:10 +0300

    add Animal enum

---
+++ FarmXpert/FarmXpert.Domain/Enums/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Enums
+{
+    public enum Animal
+    {
+        Bull,
+        Cow,
+        Heifer,
+        Steer,
+        Calf,
+    }
+}
