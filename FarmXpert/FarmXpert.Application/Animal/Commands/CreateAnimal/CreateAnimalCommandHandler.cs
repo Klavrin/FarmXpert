5f46363df097b21d0592bec0bdc60bfba3ef5b50
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 07:59:54 +0300

    feat(Application) implement CreateAnimal command and handler

---
+++ FarmXpert/FarmXpert.Application/Animal/Commands/CreateAnimal/CreateAnimalCommandHandler.cs
@@ -0,0 +1,28 @@
+using FarmXpert.Domain.Interfaces;
+using FarmXpert.Domain.Entities;
+using MediatR;
+
+namespace FarmXpert.Application.Animal.Commands.CreateAnimal;
+public class CreateAnimalCommandHandler : IRequestHandler<CreateAnimalCommand, FarmXpert.Domain.Entities.Animal>
+{
+    private readonly IAnimalRepository _animalRepository;
+
+    public CreateAnimalCommandHandler(IAnimalRepository animalRepository)
+    {
+        _animalRepository = animalRepository;
+    }
+
+    public async Task<FarmXpert.Domain.Entities.Animal> Handle(CreateAnimalCommand request, CancellationToken cancellationToken)
+    {
+        var animal = new FarmXpert.Domain.Entities.Animal
+        {
+            Id = Guid.NewGuid(),
+            CattleId = request.CattleId,
+            Species = request.Species,
+            Sex = request.Sex,
+            BirthDate = request.BirthDate
+        };
+        await _animalRepository.CreateAsync(animal, cancellationToken);
+        return animal;
+    }
+}
