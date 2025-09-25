1affa8a32ba6f3e350ea6b2bf601a316bc20649a
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 08:01:29 +0300

    feat(Application) add GetAllAnimals query and handler

---
+++ FarmXpert/FarmXpert.Application/Animal/Queries/GetAllAnimals/GetAllAnimalsQueryHandler.cs
@@ -0,0 +1,20 @@
+using MediatR;
+using FarmXpert.Domain.Interfaces;
+using FarmXpert.Domain.Entities;
+
+namespace FarmXpert.Application.Animal.Queries.GetAllAnimals;
+public class GetAllAnimalsQueryHandler : IRequestHandler<GetAllAnimalsQuery, List<FarmXpert.Domain.Entities.Animal>>
+{
+    private readonly IAnimalRepository _animalRepository;
+
+    public GetAllAnimalsQueryHandler(IAnimalRepository animalRepository)
+    {
+        _animalRepository = animalRepository;
+    }
+
+    public async Task<List<FarmXpert.Domain.Entities.Animal>> Handle(GetAllAnimalsQuery request, CancellationToken cancellationToken)
+    {
+        var animals = await _animalRepository.GetAllAsync(cancellationToken);
+        return animals.ToList();
+    }
+}
