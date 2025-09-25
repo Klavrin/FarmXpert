9c332282b3e0481ae64a174c4b6ec9870f127130
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 07:55:59 +0300

    feat(Domain) : add AnimalRepository interface

---
+++ FarmXpert/FarmXpert.Domain/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,12 @@
+﻿using FarmXpert.Domain.Entities;
+
+namespace FarmXpert.Domain.Interfaces;
+
+public interface IAnimalRepository
+{
+    Task CreateAsync(Animal animal, CancellationToken cancellationToken = default);
+    Task<Animal?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Animal>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task UpdateAsync(Animal animal, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
