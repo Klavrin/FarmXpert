70614c83202dffe39d8d90be4f1a6796e6a19d5d
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 08:41:08 +0300

    feat(Application): implement DeleteVehicle command and handler

---
+++ FarmXpert/FarmXpert.Application/Vehicle/Commands/DeleteVehicle/DeleteVehicleCommandHandler.cs
@@ -0,0 +1,27 @@
+using FarmXpert.Domain.Entities;
+using FarmXpert.Domain.Interfaces;
+using MediatR;
+
+namespace FarmXpert.Application.Vehicle.Commands.DeleteVehicle;
+
+public class DeleteVehicleCommandHandler : IRequestHandler<DeleteVehicleCommand, FarmXpert.Domain.Entities.Vehicle?>
+{
+    private readonly IVehicleRepository _vehicleRepository;
+
+    public DeleteVehicleCommandHandler(IVehicleRepository vehicleRepository)
+    {
+        _vehicleRepository = vehicleRepository;
+    }
+
+    public async Task<FarmXpert.Domain.Entities.Vehicle?> Handle(DeleteVehicleCommand request, CancellationToken cancellationToken)
+    {
+        var vehicle = await _vehicleRepository.GetByIdAsync(request.Id, cancellationToken);
+        if (vehicle == null)
+        {
+            return null;
+        }
+
+        await _vehicleRepository.DeleteAsync(request.Id, cancellationToken);
+        return vehicle;
+    }
+}
