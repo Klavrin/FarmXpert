ab64e5a9cabd5546a2edf0143dbd239ec685e2e5
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 08:38:31 +0300

    feat(Domain): add VehicleRepository interface

---
+++ FarmXpert/FarmXpert.Domain/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+using FarmXpert.Domain.Entities;
+
+namespace FarmXpert.Domain.Interfaces;
+
+public interface IVehicleRepository
+{
+    Task CreateAsync(Vehicle vehicle, CancellationToken cancellationToken = default);
+    Task<Vehicle?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Vehicle>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task UpdateAsync(Vehicle vehicle, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
