2ebf75129e2f7c1003df1347e5697ec2b0410e47
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 08:01:48 +0300

    feat(Application) add GetAnimalById query and handler

---
+++ FarmXpert/FarmXpert.Application/Animal/Queries/GetAnimalById/GetAnimalByIdQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using FarmXpert.Domain.Entities;
+
+namespace FarmXpert.Application.Animal.Queries.GetAnimalById;
+public record GetAnimalByIdQuery(Guid Id) : IRequest<FarmXpert.Domain.Entities.Animal?>;
