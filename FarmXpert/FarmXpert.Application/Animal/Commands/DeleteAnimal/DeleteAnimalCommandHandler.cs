331d014709deb37f73c3992b9ce2b59bbd684e21
Makday <58140558+Makday@users.noreply.github.com>
2025-09-25 08:00:14 +0300

    feat(Application) implement DeleteAnimal command and handler

---
+++ FarmXpert/FarmXpert.Application/Animal/Commands/DeleteAnimal/DeleteAnimalCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using FarmXpert.Domain.Interfaces;
+using MediatR;
+
+namespace FarmXpert.Application.Animal.Commands.DeleteAnimal;
+
+public class DeleteAnimalCommandHandler : IRequestHandler<DeleteAnimalCommand, FarmXpert.Domain.Entities.Animal?>
+{
+    private readonly IAnimalRepository _animalRepository;
+
+    public DeleteAnimalCommandHandler(IAnimalRepository animalRepository)
+    {
+        _animalRepository = animalRepository;
+    }
+
+    public async Task<Domain.Entities.Animal?> Handle(DeleteAnimalCommand request, CancellationToken cancellationToken)
+    {
+        var animal = await _animalRepository.GetByIdAsync(request.Id, cancellationToken);
+        if (animal == null)
+        {
+            return null;
+        }
+
+        await _animalRepository.DeleteAsync(request.Id, cancellationToken);
+        return animal;
+    }
+}
