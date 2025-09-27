6887ce2c82d90c70a50b85052201ebd2355d5bda
Makday <58140558+Makday@users.noreply.github.com>
2025-09-27 18:26:27 +0300

    feat(Application): implement CreateField command and handler

---
+++ FarmXpert/FarmXpert.Application/Field/Commands/CreateField/CreateFieldCommand.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using FarmXpert.Domain.Enums;
+
+namespace FarmXpert.Application.Field.Commands.CreateField;
+public record CreateFieldCommand(
+    Guid BusinessId,
+    CropType CropType,
+    string OtherCropType,
+    SoilType SoilType,
+    string OtherSoilType,
+    FertilizerType Fertilizer,
+    string OtherFertilizer,
+    HerbicideType Herbicide,
+    string OtherHerbicide,
+    List<double[]> Coords
+) : IRequest<FarmXpert.Domain.Entities.Field>;
