d21a616f1d73bb9a7c845d5e3c50a9fc473d69f8
Makday <58140558+Makday@users.noreply.github.com>
2025-10-05 16:41:01 +0300

    feat(Application): implement UpdatePersonalDocument command and handler

---
+++ FarmXpert/FarmXpert.Application/PersonalDocument/Commands/UpdatePersonalDocument/UpdatePersonalDocumentCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace FarmXpert.Application.PersonalDocument.Commands.UpdatePersonalDocument;
+
+public record UpdatePersonalDocumentCommand(Guid Id, string OwnerId, string Title) : IRequest<FarmXpert.Domain.Entities.PersonalDocument>;
