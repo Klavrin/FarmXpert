4470774f5950b51a539fd624769214344526c358
Makday <58140558+Makday@users.noreply.github.com>
2025-10-05 16:33:45 +0300

    feat(Infrastructure) : add FileStorageService repository

---
+++ FarmXpert/FarmXpert.Infrastructure/Repositories/FileStorageServiceRepository.cs
@@ -0,0 +1,30 @@
+﻿using FarmXpert.Domain.Interfaces;
+
+namespace FarmXpert.Infrastructure.Repositories;
+public class FileStorageServiceRepository : IFileStorageService
+{
+    private readonly string _storagePath;
+
+    public FileStorageServiceRepository(string storagePath = null)
+    {
+        _storagePath = storagePath ?? Path.Combine(Directory.GetCurrentDirectory(), "FileStorage");
+
+        if (!Directory.Exists(_storagePath))
+            Directory.CreateDirectory(_storagePath);
+    }
+
+    public async Task<string> SaveFileAsync(Stream fileStream, string fileName)
+    {
+        string fullPath = Path.Combine(_storagePath, fileName);
+        using var outputStream = new FileStream(fullPath, FileMode.Create, FileAccess.Write);
+        await fileStream.CopyToAsync(outputStream);
+
+        return fullPath;
+    }
+
+    public Task DeleteFileAsync(string fullPath)
+    {
+        if (File.Exists(fullPath)) File.Delete(fullPath);
+        return Task.CompletedTask;
+    }
+}
