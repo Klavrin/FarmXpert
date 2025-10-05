447d1b046dee613f461fc645d3e00958634c11a0
Makday <58140558+Makday@users.noreply.github.com>
2025-10-05 16:29:51 +0300

    feat(Domain) : add PersonalDocument entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/PersonalDocument.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmXpert.Domain.Entities
+{
+    public class PersonalDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Title { get; set; }
+        public DateTime UploadDate { get; set; } = DateTime.UtcNow;
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string FileExtension { get; set; }
+        public string OwnerId { get; set; } = string.Empty;
+    }
+}
