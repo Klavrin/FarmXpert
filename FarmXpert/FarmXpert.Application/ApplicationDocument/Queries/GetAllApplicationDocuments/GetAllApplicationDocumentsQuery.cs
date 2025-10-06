1191360e9edb1c1202577011518416d773499a41
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:32:14 +0300

    feat(Applicatio) : add GetAllAplicationDocuments query and handler

---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Queries/GetAllApplicationDocuments/GetAllApplicationDocumentsQuery.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+namespace FarmXpert.Application.ApplicationDocument.Queries.GetAllApplicationDocuments;
+
+public record GetAllApplicationDocumentsQuery(string OwnerId) : IRequest<List<FarmXpert.Domain.Entities.ApplicationDocument>>;
---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Queries/GetAllApplicationDocuments/GetAllApplicationDocumentsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using FarmXpert.Domain.Interfaces;
+
+namespace FarmXpert.Application.ApplicationDocument.Queries.GetAllApplicationDocuments;
+
+public class GetAllApplicationDocumentsQueryHandler : IRequestHandler<GetAllApplicationDocumentsQuery, List<FarmXpert.Domain.Entities.ApplicationDocument>>
+{
+    private readonly Domain.Interfaces.IApplicationDocumentRepository _applicationDocumentRepository;
+    public GetAllApplicationDocumentsQueryHandler(Domain.Interfaces.IApplicationDocumentRepository applicationDocumentRepository)
+    {
+        _applicationDocumentRepository = applicationDocumentRepository;
+    }
+    public async Task<List<FarmXpert.Domain.Entities.ApplicationDocument>> Handle(GetAllApplicationDocumentsQuery request, CancellationToken cancellationToken)
+    {
+        var applicationDocuments = await _applicationDocumentRepository.GetAllAsync(request.OwnerId, cancellationToken);
+        return applicationDocuments.ToList();
+    }
+}
