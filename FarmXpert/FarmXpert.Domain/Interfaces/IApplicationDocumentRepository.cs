2a6b91a2edb9b8335b8624adf633062637e52aa8
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:05:56 +0300

    refactor(Domain) : fix ApplicationDocumentRepository interface attribute bug

--- FarmXpert/FarmXpert.Domain/Interfaces/IApplicationDocumentRepository.cs
+++ FarmXpert/FarmXpert.Domain/Interfaces/IApplicationDocumentRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using FarmXpert.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FarmXpert.Domain.Interfaces
+namespace FarmXpert.Domain.Interfaces;
+public interface IApplicationDocumentRepository
 {
-    internal class Class1
-    {
-    }
+    Task CreateAsync(ApplicationDocument document, CancellationToken cancellationToken = default);
+    Task<IEnumerable<ApplicationDocument>> GetAllAsync(string OwnerId, CancellationToken cancellationToken = default);
+    Task<ApplicationDocument?> GetByIdAsync(string OwnerId, Guid id, CancellationToken cancellationToken = default);
+    Task UpdateAsync(ApplicationDocument document, CancellationToken cancellationToken = default);
+    Task DeleteAsync(string OwnerId, Guid id, CancellationToken cancellationToken = default);
 }
