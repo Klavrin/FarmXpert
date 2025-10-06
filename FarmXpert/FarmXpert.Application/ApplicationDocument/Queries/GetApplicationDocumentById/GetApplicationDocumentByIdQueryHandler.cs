812dbead9a8d95f405ebf7abbe5a7ecf28370d5f
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:35:39 +0300

    feat(Applicatio) : add GetApplicationDocumentById query and handler

---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Queries/GetApplicationDocumentById/GetApplicationDocumentByIdQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace FarmXpert.Application.ApplicationDocument.Queries.GetApplicationDocumentById;
+
+public class GetApplicationDocumentByIdQueryHandler : IRequestHandler<GetApplicationDocumentByIdQuery, FarmXpert.Domain.Entities.ApplicationDocument?>
+{
+    private readonly Domain.Interfaces.IApplicationDocumentRepository _applicationDocumentRepository;
+    public GetApplicationDocumentByIdQueryHandler(Domain.Interfaces.IApplicationDocumentRepository applicationDocumentRepository)
+    {
+        _applicationDocumentRepository = applicationDocumentRepository;
+    }
+    public async Task<FarmXpert.Domain.Entities.ApplicationDocument> Handle(GetApplicationDocumentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var applicationDocument = await _applicationDocumentRepository.GetByIdAsync(request.OwnerId, request.Id, cancellationToken);
+        return applicationDocument;
+    }
+}
