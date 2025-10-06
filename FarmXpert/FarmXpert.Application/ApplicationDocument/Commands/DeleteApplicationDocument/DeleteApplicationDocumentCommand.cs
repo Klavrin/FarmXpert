e4837df9fe186274560012c2362b77252fe37dba
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:16:27 +0300

    feat(Application) : add DeleteApplicationDocument command and handler

---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Commands/DeleteApplicationDocument/DeleteApplicationDocumentCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace FarmXpert.Application.ApplicationDocument.Commands.DeleteApplicationDocument;
+
+public record DeleteApplicationDocumentCommand() : IRequest<FarmXpert.Domain.Entities.ApplicationDocument>;
---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Commands/DeleteApplicationDocument/DeleteApplicationDocumentCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FarmXpert.Domain.Interfaces;
+using MediatR;
+
+namespace FarmXpert.Application.ApplicationDocument.Commands.DeleteApplicationDocument;
+
+public class DeleteApplicationDocumentCommandHandler : IRequestHandler<DeleteApplicationDocumentCommand, FarmXpert.Domain.Entities.ApplicationDocument?>
+{
+    private readonly IApplicationDocumentRepository _applicationDocumentRepository;
+    private readonly IFileStorageService _fileStorageServiceRepository;
+
+    public DeleteApplicationDocumentCommandHandler(IApplicationDocumentRepository applicationDocumentRepository, IFileStorageService fileStorageServiceRepository)
+    {
+        _applicationDocumentRepository = applicationDocumentRepository;
+        _fileStorageServiceRepository = fileStorageServiceRepository;
+    }
+
+    public async Task<FarmXpert.Domain.Entities.ApplicationDocument?> Handle(DeleteApplicationDocumentCommand request, CancellationToken cancellationToken)
+    {
+        var applicationDocument = await _applicationDocumentRepository.GetByIdAsync(request.OwnerId, request.id, cancellationToken);
+        if (applicationDocument == null)
+        {
+            return null;
+        }
+        var fileUrl = applicationDocument.Url;
+        if(fileUrl != null)
+        {
+            await _fileStorageServiceRepository.DeleteFileAsync(fileUrl);
+        }
+        await _applicationDocumentRepository.DeleteAsync(request.OwnerId, request.id, cancellationToken);
+        return applicationDocument;
+    }
+}
