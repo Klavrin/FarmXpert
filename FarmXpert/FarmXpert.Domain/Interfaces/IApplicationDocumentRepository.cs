42494769a65e69b2afec585d1c7f26206c3b5d66
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:31:29 +0300

    refactor(Domain) : add missing library to ApplicationDocumentRepository interface

--- FarmXpert/FarmXpert.Domain/Interfaces/IApplicationDocumentRepository.cs
+++ FarmXpert/FarmXpert.Domain/Interfaces/IApplicationDocumentRepository.cs
@@ -1,9 +1,4 @@
 ﻿using FarmXpert.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FarmXpert.Domain.Interfaces;
 public interface IApplicationDocumentRepository
