5e002c7d17af48bc63db3db1c50d097c2902dbbb
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:07:00 +0300

    feat(Application) : add CreateApplicationDocument command and handler

---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Commands/CreateApplicationDocument/CreateApplicationDocumentCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Application.ApplicationDocument.Commands.CreateApplicationDocument;
+
+public record CreateApplicationDocumentCommand(string Title, Stream FileStream, string FileExtension, string OwnerId) : IRequest<FarmXpert.Domain.Entities.ApplicationDocument>;
