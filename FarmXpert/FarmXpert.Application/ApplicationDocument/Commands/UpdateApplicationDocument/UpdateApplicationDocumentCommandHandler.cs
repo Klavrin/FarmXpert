1ea4106174f2169f5c69f13b46d8f32e5677f3b5
Makday <58140558+Makday@users.noreply.github.com>
2025-10-06 21:16:39 +0300

    feat(Application) : add UpdateApplicationDocument command and handler

---
+++ FarmXpert/FarmXpert.Application/ApplicationDocument/Commands/UpdateApplicationDocument/UpdateApplicationDocumentCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace FarmXpert.Application.ApplicationDocument.Commands.UpdateApplicationDocument;
+
+public class UpdateApplicationDocumentCommandHandler : IRequestHandler<UpdateApplicationDocumentCommand, FarmXpert.Domain.Entities.ApplicationDocument?>
+{
+    private readonly Domain.Interfaces.IApplicationDocumentRepository _applicationDocumentRepository;
+    public UpdateApplicationDocumentCommandHandler(Domain.Interfaces.IApplicationDocumentRepository applicationDocumentRepository)
+    {
+        _applicationDocumentRepository = applicationDocumentRepository;
+    }
+    public async Task<FarmXpert.Domain.Entities.ApplicationDocument?> Handle(UpdateApplicationDocumentCommand request, CancellationToken cancellationToken)
+    {
+        var applicationDocument = await _applicationDocumentRepository.GetByIdAsync(request.OwnerId, request.Id, cancellationToken);
+        if (applicationDocument == null)
+        {
+            return null;
+        }
+        applicationDocument.Title = request.Title;
+        applicationDocument.Status = request.Status;
+        applicationDocument.RejectionReason = request.ReasonRejection;
+        await _applicationDocumentRepository.UpdateAsync(applicationDocument, cancellationToken);
+        return applicationDocument;
+    }
+}
