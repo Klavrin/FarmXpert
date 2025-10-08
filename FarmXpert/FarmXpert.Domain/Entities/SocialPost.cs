ff4dff7dca977eb235f80ef456c32d0ae59b270f
Makday <58140558+Makday@users.noreply.github.com>
2025-10-08 09:23:12 +0300

    feat(Domain) : add SocialPost entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/SocialPost.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmXpert.Domain.Entities;
+
+public class SocialPost
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Url { get; set; }
+
+    [Required]
+    public string OwnerId { get; set; }
+
+    public DateTime UploadDate { get; set; } = DateTime.UtcNow;
+
+    public string Content { get; set; } = string.Empty;
+
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+    public int LikesCount { get; set; } = 0;
+    public int CommentsCount { get; set; } = 0;
+}
