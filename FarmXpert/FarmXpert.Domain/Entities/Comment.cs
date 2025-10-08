5db4b12414f27c00ebc57f9082a48c9ce7259c55
Makday <58140558+Makday@users.noreply.github.com>
2025-10-08 09:22:59 +0300

    feat(Domain) : add Comment entity

---
+++ FarmXpert/FarmXpert.Domain/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmXpert.Domain.Entities;
+
+public class Comment
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+}
