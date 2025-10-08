e63818142322929406a6e741118b03ebfcaf49aa
Makday <58140558+Makday@users.noreply.github.com>
2025-10-08 10:58:59 +0300

    feat(Application) : implement EditSocialPost command and handler

---
+++ FarmXpert/FarmXpert.Application/SocialPost/Commands/EditSocialPost/EditSocialPostCommand.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+
+namespace FarmXpert.Application.SocialPost.Commands.EditSocialPost;
+public record EditSocialPostCommand(Guid Id, string Title, string Content) : IRequest<FarmXpert.Domain.Entities.SocialPost>;
