82aacc3513ae2846ee6ee4f875355ee598283bb0
Makday <58140558+Makday@users.noreply.github.com>
2025-10-08 09:25:55 +0300

    feat(Interfaces) : add SocialPostRepository interface

---
+++ FarmXpert/FarmXpert.Domain/Interfaces/ISocialPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmXpert.Domain.Interfaces;
+
+public interface ISocialPostRepository
+{
+    Task CreateAsync(Entities.SocialPost post, CancellationToken cancellationToken = default);
+    Task<Entities.SocialPost?> GetByIdAsync(string OwnerId, Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Entities.SocialPost>> GetAllAsync(string OwnerId, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Entities.SocialPost post, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+}
