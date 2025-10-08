655988727de53b3dcf80768c75678e292f991ee1
Makday <58140558+Makday@users.noreply.github.com>
2025-10-08 15:43:05 +0300

    feat(Application) : add GetUserSocialPost query

---
+++ FarmXpert/FarmXpert.Application/SocialPost/Queries/GetUserSocialPost/GetUserSocialPostQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using FarmXpert.Domain.Interfaces;
+using MediatR;
+
+namespace FarmXpert.Application.SocialPost.Queries.GetUserSocialPost;
+
+public class GetUserSocialPostQueryHandler : IRequestHandler<GetUserSocialPostQuery, List<FarmXpert.Domain.Entities.SocialPost>>
+{
+    private readonly ISocialPostRepository _socialPostRepository;
+    public GetUserSocialPostQueryHandler(ISocialPostRepository socialPostRepository)
+    {
+        _socialPostRepository = socialPostRepository;
+    }
+    public async Task<List<FarmXpert.Domain.Entities.SocialPost>> Handle(GetUserSocialPostQuery request, CancellationToken cancellationToken)
+    {
+        var posts = await _socialPostRepository.GetAllByUserAsync(request.BusinessId, cancellationToken);
+        return posts.ToList();
+    }
+}
