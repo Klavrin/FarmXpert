bb01e6e991f128c2e85a68b3819a9feaa49f3ef0
Makday <58140558+Makday@users.noreply.github.com>
2025-10-08 09:34:52 +0300

    refactor(Domain) : add BusinessId attribute to Comment entity

--- FarmXpert/FarmXpert.Domain/Entities/Comment.cs
+++ FarmXpert/FarmXpert.Domain/Entities/Comment.cs
@@ -13,7 +13,8 @@ public class Comment
     public string FirstName { get; set; }
     [Required]
     public string LastName { get; set; }
-
     [Required]
     public string Content { get; set; }
+    [Required]
+    public string businessId { get; set; }
 }
