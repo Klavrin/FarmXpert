8689a7c1e73fa2e23ac64c9889bbede0afa61b20
Makday <58140558+Makday@users.noreply.github.com>
2025-11-02 14:01:50 +0200

    feat(Tests) : add GetAllVehiclesQuery unit tests

---
+++ FarmXpert/FarmXpert.Tests/Application/Vehicle/Queries/GetAllVehiclesQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using FarmXpert.Application.Vehicle.Queries.GetAllVehicles;
+using FarmXpert.Domain.Interfaces;
+using FluentAssertions;
+
+namespace FarmXpert.Tests.Application.Vehicle.Queries;
+public class GetAllVehiclesQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_ReturnsAllVehiclesForOwner()
+    {
+        // Arrange
+        var mockRepo = new Mock<IVehicleRepository>();
+        var expectedVehicles = new List<Domain.Entities.Vehicle>
+        {
+            new Domain.Entities.Vehicle { Id = Guid.NewGuid(), OwnerId = "owner123" },
+            new Domain.Entities.Vehicle { Id = Guid.NewGuid(), OwnerId = "owner123" }
+        };
+        mockRepo.Setup(r => r.GetAllAsync("owner123", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedVehicles);
+
+        var handler = new GetAllVehiclesQueryHandler(mockRepo.Object);
+        var query = new GetAllVehiclesQuery("owner123");
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedVehicles);
+    }
+}
