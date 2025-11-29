bbb4d282d5a62b4e01592b341e228f9037da1afd
Makday <58140558+Makday@users.noreply.github.com>
2025-11-29 13:58:31 +0200

    refactor(Application): allow null return for DeleteLikeCommandHandle

--- FarmXpert/FarmXpert.Application/SocialPost/Commands/DeleteLike/DeleteLikeCommand.cs
+++ FarmXpert/FarmXpert.Application/SocialPost/Commands/DeleteLike/DeleteLikeCommand.cs
@@ -1,4 +1,4 @@
 using MediatR;
 
 namespace FarmXpert.Application.SocialPost.Commands.DeleteLike;
-public record DeleteLikeCommand(Guid id, string BusinessId) : IRequest<FarmXpert.Domain.Entities.SocialPost>;
+public record DeleteLikeCommand(Guid id, string BusinessId) : IRequest<FarmXpert.Domain.Entities.SocialPost?>;
